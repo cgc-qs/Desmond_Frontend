cede5f5b769b5dfbecc1b6403d216b7b5ee2d1cc
cgc-qs <87303775+cgc-qs@users.noreply.github.com>
2023-09-07 03:02:23 +0900

    table

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import urlContext from '../URLContext'
 import axios from 'axios';
+import InforTable from "../standardTable_NEW";
 
 export default function HomePage() {
 
-    const [accountInfo, setAccountInfo] = useState();
+    const [accountInfo, setAccountInfo] = useState([]);
 
     const baseURL = React.useContext(urlContext);
-
+    const [activeTradeID, setActiveTradeID] = useState(0);
+    const [deleteTradeSettingRow, setDeleteTradeSettingRow] = useState(false);
     useEffect(() => {
 
         const timer = window.localStorage.getItem('timer');
@@ -24,7 +26,7 @@ export default function HomePage() {
             axios(config)
                 .then(function (res) {
                     console.log(res.data);
-
+                    setAccountInfo(res.data);
                 })
                 .catch(function (err) {
                     console.log('error=', err);
@@ -37,13 +39,43 @@ export default function HomePage() {
         }
 
     });
+    const headername = [
+        { id: "brokerName", numeric: false, label: "brokerName" },
+        { id: "accountNumber", numeric: false, label: "accountNumber" },
+        { id: "currentEquity", numeric: false, label: "currentEquity" },
+        { id: "threshold", numeric: false, label: "threshold" },
+        { id: "topUpAmount", numeric: false, label: "topUpAmount" },
+        { id: "activeStatus", numeric: false, label: "activeStatus" },
+        { id: "totalSwap", numeric: false, label: "totalSwap" },
+        { id: "longSwap", numeric: false, label: "longSwap" },
+        { id: "shortSwap", numeric: false, label: "shortSwap" },
+        { id: "edit", numeric: false, label: " " },
+        //{ id: "delete", numeric: false, label: " " },
+
+    ];
+
+    const signUP = () => {
+        localStorage.setItem('accessToken', "");
+    }
     return (
-        <div style={{ display: "flex", justifyContent: "Space-between" }}>
-            <h1 className="main-title2 home-page-title" style={{ marginLeft: 50 }}>Scanner table</h1>
-            <div >
-                <Link to="/">
-                    <button className="primary-button" style={{ marginTop: "2rem" }}>Log out</button>
-                </Link>
+        <div>
+            <div style={{ display: "flex", justifyContent: "Space-between" }}>
+                <h1 className="main-title2 home-page-title" style={{ marginLeft: 50 }}>Scanner table</h1>
+                <div >
+                    <Link to="/">
+                        <button className="primary-button" style={{ marginTop: "2rem" }} onClick={signUP}>Log out</button>
+                    </Link>
+                </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <div style={{
+                    border: '2px solid rgba(0, 0, 0, 0.1)', borderRadius: 5, marginLeft: 10, marginTop: 50,
+                    width: 'calc(90%)', marginRight: 10, padding: 5
+                }}>
+                    <InforTable targets={accountInfo} setTargets={setAccountInfo} headername={headername}
+                        activeName={activeTradeID} setActiveName={setActiveTradeID} SetDeleteRow={setDeleteTradeSettingRow}
+                        disabledTable={false} />
+                </div>
             </div>
         </div>
 
