4fb97c3caceeb0ea25bac9a98000014708928efc
cgc-qs <87303775+cgc-qs@users.noreply.github.com>
2023-09-13 07:56:35 +0000

    Final

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { HashRouter  } from "react-router-dom";
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import App from "./App";
+// import * as serviceWorker from "./service-worker";
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <HashRouter >
+//     <App />
+//   </HashRouter >,
+//   document.getElementById("root")
+// );
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
